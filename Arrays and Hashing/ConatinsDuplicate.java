4716882ab0bc4da02140b3f4214a7ba944099de9
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2025-12-14 23:58:50 +0530

    Create ConatinsDuplicate.java

---
+++ Arrays and Hashing/ConatinsDuplicate.java
@@ -0,0 +1,18 @@
+Problem Link: https://leetcode.com/problems/contains-duplicate/
+
+
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> ans = new HashSet<>();
+        if (nums.length == 1)
+            return false;
+        for (int i = 0; i < nums.length; i++) {
+            if (ans.contains(nums[i])) {
+                return true;
+            } else
+                ans.add(nums[i]);
+        }
+        return false;
+    }
+}
