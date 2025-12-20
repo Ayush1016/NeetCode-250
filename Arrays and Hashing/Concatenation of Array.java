ee5815cc18c3ed6a291bc9e550ca8adc1898daed
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2025-12-20 17:43:09 +0530

    Create Concatenation of Array.java

---
+++ Arrays and Hashing/Concatenation of Array.java
@@ -0,0 +1,11 @@
+Problem Link: https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]= new int[nums.length*2];
+        for(int i=0;i<ans.length;i++){
+             ans[i] = nums[i % nums.length];
+        }
+        return ans;
+    }
+}
