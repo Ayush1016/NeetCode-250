dce993b26979afa2b5b667c1c2af5958d29fee59
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2026-01-10 23:54:59 +0530

    Create Majority Element.java

---
+++ Arrays and Hashing/Majority Element.java
@@ -0,0 +1,22 @@
+Problem Link: https://leetcode.com/problems/majority-element/submissions/1881016166/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+         HashMap<Integer, Integer> map = new HashMap<>();
+        int freq=0;
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>nums.length/2){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
